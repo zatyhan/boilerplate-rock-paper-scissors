6313debc955f37a32cba8e9af82226941b735699
Nur Eizzaty Hamzaid <nureizzaty@gmail.com>
2024-02-17 21:37:59 +0000

    added custom algorithm for RPS

--- RPS.py
+++ RPS.py
@@ -1,10 +1,28 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
-def player(prev_play, opponent_history=[]):
+import random
+def player(prev_play, opponent_history=[], play_history={}):
+    hist= 5
+    if not prev_play:
+        prev_play= 'R'
     opponent_history.append(prev_play)
+    opponent_response= random.choice(['R', 'P', 'S'])
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    last_few = "".join(opponent_history[-hist:])
 
-    return guess
+    if len(opponent_history)> hist+1:
+        past_play= "".join(opponent_history[-hist-1:])
+        play_history[past_play]=play_history.get(past_play, 0) + 1
+        # print(play_history)
+        # print(opponent_history)
+
+    if len(last_few)==hist:
+        possible_outcome={}
+        combinations= [last_few+k for k in ['R', 'P', 'S']]
+        for c in combinations:
+            possible_outcome[c]= play_history.get(c, 0)
+        opponent_response= max(possible_outcome, key= possible_outcome.get)[-1]
+        # print(possible_outcome)
+        # print(possible_outcome)
+    
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[opponent_response] 
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from RPS import player
 from unittest import main
 
 play(player, quincy, 1000)
-play(player, abbey, 1000)
 play(player, kris, 1000)
+play(player, abbey, 5000)
 play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
@@ -17,4 +17,4 @@ play(player, mrugesh, 1000)
 
 
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
